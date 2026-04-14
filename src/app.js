dea9e95e43d644d3aeeddaeba516ebf362565c28
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-04-13 20:06:31 -0500

    feat: habilita CORS en backend para permitir consumo desde frontend

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "./config/db.js";
 import userRouter from "./routes/usuario.routes.js";
 import categoryRouter from "./routes/categoria.routes.js";
@@ -7,6 +8,7 @@ import auditRouter from "./routes/auditoria.routes.js";
 import { successResponse } from "./utils/response.handler.js";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
